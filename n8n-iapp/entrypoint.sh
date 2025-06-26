b9b484dcb5214071bad6d4668c9481b1103c66e8
wlf <adewulf@iex.ec>
2025-06-26 18:41:52 +0200

    add computed.json support

--- n8n-iapp/entrypoint.sh
+++ n8n-iapp/entrypoint.sh
@@ -7,6 +7,9 @@ node src/app.js
 
 n8n execute --id "N43f6oGefV50KGZX"
 
+cat result.txt > /iexec_out/result.txt
+echo '{"deterministic-output-path": "/iexec_out/result.txt"}' > /iexec_out/computed.json
+
 #n8n start
 
 #sleep 300
